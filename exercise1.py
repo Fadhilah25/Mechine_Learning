305d5fe2146bba0e8b0fa817033d7c695ce1dd50
FadhilahAA <112860183+Fadhilah25@users.noreply.github.com>
2024-03-25 20:37:44 +0700

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,23 @@
+# Fadhilah Aditya Akbar
+# A11.2022.14130
+# LATIHAN 1
+
+print("|| Python Basic ||")
+print("Hello World!")
+print("test\n")
+
+print("|| Latihan The Print Function ||")
+print("Hello, Python!")
+print("Fadhil")
+print('Fadhil')
+
+print("|| Formatting with Special Character ||")
+print()
+print("Saya tadi pagi sahur\n dan nanti sore buka bersama") # \n artinya enter
+print("Saya tadi pagi sahur\t dan nanti sore buka bersama") # \t artinya tab
+print("nama saya \"Fadhil\"")
+print("\"")
+print("satu.","dua.","tiga.")
+print("Nama saya","Fadhil", end=" ")
+print("Salam")
+print("satu","dua","tiga", sep=", ")
